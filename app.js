1b1a8cc5d5404d4da51e2930ac30e3c661dc15b5
Kike-GB <enrique.gonzalez.boselli@hotmail.com>
2020-09-28 07:16:05 -0300

    se incorpora app.js y el resto de las páginas de prueba para el app.js

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-const express = require("express");
+const express = require('express');
+const app = express();
+var path = require("path")
 
-console.log (express);
+app.get('/', (req, res) => {
+    let file = path.resolve('./site/view/index.html')
+    res.sendFile(file);
+});
+app.get('/login', (req, res) => {
+    let file = path.resolve('./site/view/login.html')
+    res.sendFile(file);
+});
+app.get('/productCart', (req, res) => {
+    let file = path.resolve('./site/view/productCart.html')
+    res.sendFile(file);
+});
+app.get('/productDetail', (req, res) => {
+    let file = path.resolve('./site/view/productDetail.html')
+    res.sendFile(file);
+});
+app.get('/register', (req, res) => {
+    let file = path.resolve('./site/view/register.html')
+    res.sendFile(file);
+});
+
+app.get('*', (req, res) => {
+    if (req.url.endsWith('.css')) {
+        let file = path.resolve('./site/public/style' + req.url);
+        return res.sendFile(file); 
+    }
+    let images = ['jpg', 'jpeg', 'gif', 'png', 'svg'];
+    let extencion = req.url.split('.')[1];
+    if (images.includes(extencion)) {
+        let file = path.resolve('./site/public/images' + req.url);
+        return res.sendFile(file)
+    }
+    res.send('File Not Found!!!')
+}).listen(3000, 'localhost', () => console.log('Server ON!!! running in port 3000'));
