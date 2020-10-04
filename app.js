0ea9dcd1abc9fe2dc6c143f86565646cdc6b75b7
santiBernstein <santiago.bernstein@gmail.com>
2020-10-04 20:58:59 -0300

    vista products, quienes-somos. html y css

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.get('/register', (req, res) => {
     let file = path.resolve('./site/view/register.html')
     res.sendFile(file);
 });
+app.get('/quienes-somos', (req, res) => {
+    let file = path.resolve('./site/view/quienes-somos.html')
+    res.sendFile(file);
+});
+app.get('/products', (req, res) => {
+    let file = path.resolve('./site/view/products.html')
+    res.sendFile(file);
+});
 
 app.get('*', (req, res) => {
     if (req.url.endsWith('.css')) {
