0f1e545c99dac3f1215cf340ae668fb9ff4588eb
Kike-GB <enrique.gonzalez.boselli@hotmail.com>
2020-12-01 21:10:35 -0300

    rutas para admin y guest, retoques varios

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use(session(
 ));
 app.use(function(req,res,next){
   if(req.session.user != undefined){
-    res.locals.user = req.session.user   
+    res.locals.user = req.session.user
+    res.locals.tipoUsuario = req.session.tipoUsuario   
   }  next()
 })
 app.use(remember)
