4ddc324081070e408e2af3788e00efa250ab6238
Kike-GB <enrique.gonzalez.boselli@hotmail.com>
2021-02-21 16:57:33 -0300

    mejoramiento del validador frontend del formulario de registro

--- public/script/register.js
+++ public/script/register.js
@@ -4,27 +4,33 @@ window.addEventListener("load", () => {
     let name = form.name
     let email = form.email
     let password = form.password  
+    let confirmPassword = form.confirmPassword
     let errors = {}  
 
     form.addEventListener("submit", (e)=> {
         if(Object.keys(errors).length > 0){
             e.preventDefault(); 
         }
-         
+    });
+    name.addEventListener("keyup", ()=> {
         if (name.value.length < 6 || name.value.length == 0) {
-             name.style.border = "2px solid red"
-             errors[name]="name"
-        } else {
-            name.style.border = "0px solid white"
-            delete errors[name] 
-        }
-        if (email.value.length < 4 || email.value.length == 0) {
+            name.style.border = "2px solid red"
+            errors[name]="name"
+       } else {
+           name.style.border = "0px solid white"
+           delete errors[name] 
+       }
+    });
+    email.addEventListener("keyup", ()=> {
+        if (!validator.isEmail(email.value,)) {
             email.style.border = "2px solid red"
             errors[email]="email"
         } else {
             email.style.border = "0px solid white"
             delete errors[email]
         }
+    });
+    password.addEventListener("keyup", ()=> {
         if (password.value.length < 8 || password.value.length == 0) {
             password.style.border = "2px solid red"
             errors[password]="password"
@@ -32,5 +38,14 @@ window.addEventListener("load", () => {
             password.style.border = "0px solid white"
             delete errors[password]
         }
-    })
+    });
+    confirmPassword.addEventListener("keyup", ()=> {
+        if (confirmPassword.value.length < 8 || confirmPassword.value.length == 0) {
+            confirmPassword.style.border = "2px solid red"
+            errors[confirmPassword]="confirmPassword"
+        } else {
+            confirmPassword.style.border = "0px solid white"
+            delete errors[confirmPassword]
+        }
+    });
 })
