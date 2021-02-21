c1bfb45b36f44d95b0e914057d81170a84139398
santiBernstein <santiago.bernstein@gmail.com>
2021-02-21 18:58:10 -0300

    rutas, controlador y modificacion de vistas para agregar producto a carrito

--- controllers/carritocontrollers.js
+++ controllers/carritocontrollers.js
@@ -1,10 +1,68 @@
-
-let fs = require('fs');
+const db = require('../database/models');
+const {validationResult} = require('express-validator');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
-    cart : (req, res) => {
-        res.render('carrito/carrito')
+
+    cart : (req, res) => {  
+
+        db.Product.findAll({
+          where: {
+            // userId: req.session.user.id,
+            // state: 1,
+          },
+          include: [{association:"category"}, {association:"style"}, {association:"colores"}],
+        }).then((productsData) => {
+          return res.render('carrito/carrito', {productsData})
+        });
     },
+
+    addToCart : (req, res) => {
+        const errors = validationResult(req);
+    
+        if (errors.isEmpty()) {
+          // Busco el producto que voy a agregar como Item.
+          db.Product.findByPk(req.body.productId, {
+            include: [{association:"category"}, {association:"style"}, {association:"colores"}],
+          })
+            .then((product) => {
+                  
+              // Creo el Item de compra
+              return productsData.create({
+                // salePrice: price,
+                // quantity: req.body.quantity,
+                // subTotal: price * req.body.quantity,
+                // state: 1,
+                // userId: req.session.user.id,
+                // sellerId: product.user.id,
+                // productId: product.id,
+              });
+            })
+            .then((item) => res.redirect("/carrito/carrito"))
+            .catch((e) => console.log(e));
+        } else {
+           Product.findByPk(req.body.productId, {
+            include: [{association:"category"}, {association:"style"}, {association:"colores"}],
+            
+           })
+             .then(product => {
+                return res.render('products/detail', {product, errors: errors.mapped()})
+             })
+        }
+    },
+
+    deleteFromCart : (req, res) => {
+        Item.destroy({
+          where: {
+            id: req.body.itemId,
+          },
+          force: true,
+        })
+          .then((response) => res.redirect("/carrito/carrito"))
+          .catch((e) => console.log(e));
+      },
+
     send : (req, res) => {
         res.render('carrito/send')
     }
--- routes/carrito.js
+++ routes/carrito.js
@@ -3,6 +3,11 @@ var router = express.Router();
 const carritoControllers = require('../controllers/carritocontrollers');
 
 router.get('/', carritoControllers.cart);
+
+router.post('/addToCart', carritoControllers.addToCart);
+router.post('/deleteFromCart', carritoControllers.deleteFromCart);
+
+
 router.get('/Send', carritoControllers.send);
 
 module.exports = router;
